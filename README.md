546aa13bd053811eed5bac826fdbdd44e61ab2ea
Elyzaveta Perekhrestenko <ipz241_pyei@student.ztu.edu.ua>
2026-06-16 14:17:50 +0300

    Update API code and add Postman documentation links to README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PHP Frameworks 
+# Завдання 8: API на Laravel та Symfony
+
+- [Документація Laravel API](https://documenter.getpostman.com/view/55943009/2sBXwtpUov)
+- [Документація Symfony API](https://documenter.getpostman.com/view/55943009/2sBXwtpUox)
--- symfony/src/Controller/CategoryController.php
+++ symfony/src/Controller/CategoryController.php
@@ -5,17 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 
-class PlaneController extends AbstractController
+
+class CategoryController extends AbstractController
 {
-    #[Route('/api/planes', name: 'get_planes', methods: ['GET'])]
+
     public function index(): JsonResponse
     {
         return $this->json([
-            'message' => 'Список літаків',
+            'message' => 'Список категорій',
             'data' => [
-                ['id' => 1, 'model' => 'Boeing 747'],
-                ['id' => 2, 'model' => 'Airbus A380']
+                ['id' => 1, 'name' => 'Електроніка'],
+                ['id' => 2, 'name' => 'Одяг']
             ]
         ]);
     }
